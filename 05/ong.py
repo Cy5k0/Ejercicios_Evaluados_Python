0874275071d3a4a76fae34f1345e0f21e7f273b4
Cy5k0 <francisco.colomer@gmail.com>
2024-08-12 16:37:56 -0400

    actualizacion de readme y anotaciones ong.py

--- 05/ong.py
+++ 05/ong.py
@@ -1,4 +1,10 @@
+# Apoyo Matemático
+
 import sys
+
+# función para calcular factorial y comprobar que no sea números negativos, 
+# usando el metodo range el cual define el el indice en el que comienza,
+# y el útlimo indice , este no se toma en cuenta por eso se suma 1.
 
 def factorial(n_fact):
     if n_fact < 0:
@@ -8,6 +14,7 @@ def factorial(n_fact):
         resultado *= i
     return resultado
 
+# función para calcular la productoria y valida que esta lista no esté vacía
 def productoria(lista):
     if not lista:
         return "La lista está vacía"
@@ -16,22 +23,23 @@ def productoria(lista):
         resultado *= num
     return resultado
 
-# Verificar si se proporcionaron los argumentos correctos
+# Verifica si se ingresaron los argumentos correctos
 if len(sys.argv) < 3:
     print("Uso: python script.py <lista_numeros> <numero_factorial>")
     print("Ejemplo: python script.py 1,2,3,4 5")
     sys.exit(1)
 
-# Obtener la lista de números y el número para el factorial desde sys.argv
+# Obtiene la lista de números y el número para el factorial desde la linea de comando
 lista_str = sys.argv[1]
 lista_num = [int(num.strip()) for num in lista_str.split(',') if num.strip()]
 
+# verifica que el número ingresado sea correcto
 try:
     nro_factorial = int(sys.argv[2])
 except ValueError:
     print("El número para el factorial debe ser un entero válido.")
     sys.exit(1)
 
-# Cálculo y muestra de resultados
+
 print(f"El factorial de {nro_factorial} es: {factorial(nro_factorial)}")
 print(f"La productoria de la lista {lista_num} es: {productoria(lista_num)}")
